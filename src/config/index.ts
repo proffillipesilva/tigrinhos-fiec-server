96aa59ce0b79fdfc74cea83b9f27d4aa08b3fd59
Fillipe de Souza Silva <fssilva@fiecalunos.lan>
2024-04-22 21:03:46 -0300

    Resolvendo conflitos

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,9 @@ import cors from 'cors'
 import morgan from 'morgan'
 import usuarioRouter from '../routes/usuarioRouter';
 import gameRouter from '../routes/gameRouter';
-
+import { ExtractJwt, Strategy as JwtStrategy, StrategyOptionsWithRequest } from 'passport-jwt';
+import passport from 'passport';
+import jwt from 'jsonwebtoken'
 
 
 const app = express();
@@ -11,7 +13,44 @@ const app = express();
 app.use(cors());
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'))
 app.use(express.json())
-app.use('/usuarios', usuarioRouter);
+
+const jwtSecret = process.env.JWT_SECRET || 'secret'
+
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+    secretOrKey:  jwtSecret,
+    passReqToCallback: true
+  } ;
+
+  passport.use(new JwtStrategy(jwtOptions as StrategyOptionsWithRequest, async ( req: Request, jwtPayload, done) => {
+    const url = req.method + ' ' + req.baseUrl + req.url
+    console.log("user: ", jwtPayload.sub, " - url: - ", url )
+    if (jwtPayload.type === 0) {
+      done(null, { id: 'user123' });  // error first pattern
+    } else {
+      done(null, false);
+    }
+  }));
+
+
+  app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    if (username === 'user123' && password === 'password123') {
+      const token = jwt.sign({ sub: 'john@mail.com', type: 0 },  jwtSecret,
+      {
+        expiresIn: '10m'
+      });
+      res.json({ token });
+    } else {
+      res.status(401).send('Invalid credentials');
+    }
+  });
+
+
+
+app.use('/usuarios', passport.authenticate('jwt', { session: false }), usuarioRouter);
+
+
 app.use('/games', gameRouter);
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
 import dotenv from 'dotenv'
 import http from 'http';
 import app from './config';
-import { AppDataSource } from './config/data-source';
+//import { AppDataSource } from './config/data-source';
 import connectDB from './config/mongo-source';
+import { AppDataSource } from './config/data-source';
 
 const nomeApp = ["Tigrinhos Fiec 2024"]
 
-let x = '5';
 
-//dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
-dotenv.config({ path: `.env.local` })
+
+const environment = (process.env.NODE_ENV || "local").trim();
+
+dotenv.config({ path: `.env.${environment}` })
+//dotenv.config({ path: `.env.local` })
 
 console.log(`${nomeApp} vai rodar na porta ${process.env.PORT}`);
 
 const server = http.createServer(app);
 
-
 AppDataSource.initialize().then(() => {
     server.listen(process.env.PORT, () => "Servidor Inicializado");
 })
+
 /*
 connectDB().then( ()=> {
     server.listen(process.env.PORT, () => "Servidor Inicializado");
-}) */
-
+})*/
